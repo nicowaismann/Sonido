6e013ae75175a92f7978f07276ea371aeb3c9795
nicowaismann <46961446@est.ort.edu.ar>
2022-05-31 08:43:33 -0300

    Initial commit

---
+++ Assets/Sounds/PlaySounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+
+    public AudioClip deathSound;
+    public AudioClip jumpSound;
+
+    AudioSource fuenteAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteAudio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fuenteAudio.clip = jumpSound;
+            fuenteAudio.Play();
+        }
+
+
+
+    }
+}
